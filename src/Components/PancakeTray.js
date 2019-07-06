c8f25b1203f74d26afcabb5dac168b4a2afdff85
Reed MacDonald <reedpmacdonald@gmail.com>
2019-07-06 10:33:35 -0700

    hmmm idk

--- src/Components/PancakeTray.js
+++ src/Components/PancakeTray.js
@@ -6,6 +6,7 @@ class PancakeTray extends Component{
   render(){
     return(
   < >
+    <></>
     <img onClick={this.props.addPancakes} id='pancakeTray' src='https://www.pngarts.com/files/3/Pancake-PNG-Transparent-Image.png'/>
   </>
     )
