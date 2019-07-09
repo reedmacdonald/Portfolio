7bb1af1dc6a8e84fb524068f19595cdf1934c74c
Reed MacDonald <reedpmacdonald@gmail.com>
2019-07-08 22:33:21 -0700

    change syrup Index

--- src/Components/Syrup.js
+++ src/Components/Syrup.js
@@ -18,7 +18,8 @@ class Syrup extends Component{
             skillsHeight:'250px',
             skillsWidth:'200px',
             skillsTop:'375px',
-            skillsLeft:'500px'
+            skillsLeft:'500px',
+            syrupIndex:'700'
         })
     }
     resetSyrup=()=>{
@@ -30,15 +31,16 @@ class Syrup extends Component{
             skillsHeight:undefined,
             skillsWidth:undefined,
             skillsTop:undefined,
-            skillsLeft:undefined
+            skillsLeft:undefined,
+            syrupIndex:undefined
         })
     }
 
   render(){
     return(
   <>
-    <img onDoubleClick={this.props.pourSyrup} onClick={!this.state.syrupHeight?this.moveSyrup:this.resetSyrup} style={{height:this.state.syrupHeight,width:this.state.syrupWidth,top:this.state.syrupTop,left:this.state.syrupLeft}} id='syrup' src='http://sipplssugarbush.com/wp-content/uploads/2017/12/Maple-Syrup-Bottle-With-Handle.png'/>
-    <div onDoubleClick={this.props.pourSyrup} onClick={!this.state.syrupHeight?this.moveSyrup:this.resetSyrup} id="skills" style={{height:this.state.skillsHeight,width:this.state.skillsWidth,top:this.state.skillsTop,left:this.state.skillsLeft}}>
+    <img onDoubleClick={this.props.pourSyrup} onClick={!this.state.syrupHeight?this.moveSyrup:this.resetSyrup} style={{height:this.state.syrupHeight,width:this.state.syrupWidth,top:this.state.syrupTop,left:this.state.syrupLeft,zIndex:this.state.syrupIndex}} id='syrup' src='http://sipplssugarbush.com/wp-content/uploads/2017/12/Maple-Syrup-Bottle-With-Handle.png'/>
+    <div onDoubleClick={this.props.pourSyrup} onClick={!this.state.syrupHeight?this.moveSyrup:this.resetSyrup} id="skills" style={{height:this.state.skillsHeight,width:this.state.skillsWidth,top:this.state.skillsTop,left:this.state.skillsLeft,zIndex:this.state.syrupIndex}}>
         {!this.state.syrupHeight?<h3>Skills<h6>Double click to pour (cs)Syrup</h6></h3>:<><h3><u>Skills</u></h3><ul>
             <li>React</li>
             <li>JavaScript</li>
